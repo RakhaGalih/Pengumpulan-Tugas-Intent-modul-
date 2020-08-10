787497fa79dd1b1253e7e8a5beaf24f7471cdeba
RakhaGalih <54633534+RakhaGalih@users.noreply.github.com>
2020-08-10 19:58:38 +0700

    Add files via upload

---
+++ myintentapp/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.myintentapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btnMoveActivity = findViewById(R.id.btn_move_activity);
+        Button btnMoveWithDataActivity = findViewById(R.id.btn_move_activity_data);
+        Button btnDialPhone = findViewById(R.id.btn_dial_number);
+
+        btnMoveActivity.setOnClickListener(this);
+        btnMoveWithDataActivity.setOnClickListener(this);
+        btnDialPhone.setOnClickListener(this);
+    }
+    @Override
+    public void onClick (View v){
+        switch (v.getId()){
+            case R.id.btn_move_activity:
+                Intent moveIntent = new Intent(MainActivity.this, MoveActivity.class);
+                startActivity(moveIntent);
+                break;
+            case R.id.btn_move_activity_data:
+                Intent moveWithDataIntent = new Intent(MainActivity.this, MoveWithData.class);
+                moveWithDataIntent.putExtra(MoveWithData.EXTRA_NAME, "hai");
+                moveWithDataIntent.putExtra(MoveWithData.EXTRA_AGE, 5);
+                startActivity(moveWithDataIntent);
+                break;
+            case R.id.btn_dial_number:
+                String phoneNumber = "081210841382";
+                Intent dialPhone = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:"+phoneNumber));
+                startActivity(dialPhone);
+                break;
+        }
+    }
+
+}
